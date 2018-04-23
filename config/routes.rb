b7ea9db670b9a1e1f1fae4357b5d26bf39ede04f
groves54 <groves54@marshall.edu>
2018-04-23 18:35:14 -0400

    Initial CSS

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'home/index'
 
   root 'home#index'
+
+  get '/index', to: 'home#index'
+  get '/student', to: 'students#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
